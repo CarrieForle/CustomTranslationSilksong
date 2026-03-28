7c1b0bfecbbd35b4708ae0c36c3a47b8b9836620
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2026-03-28 23:31:49 +0800

    add reloading translation

--- CustomTranslation/ModMenu.cs
+++ CustomTranslation/ModMenu.cs
@@ -2,6 +2,8 @@ using System.Diagnostics;
 using Silksong.ModMenu.Elements;
 using Silksong.ModMenu.Plugin;
 using Silksong.ModMenu.Screens;
+using TeamCherry.Localization;
+using UnityEngine.UI;
 
 namespace CustomTranslation;
 
@@ -16,13 +18,35 @@ public partial class CustomTranslationPlugin: IModMenuCustomElement
 	{
 		SimpleMenuScreen menu = new(Name);
 		var openDirectoryBtn = new TextButton(Text.Localized("OPTION_OPEN_TRANSLATION_DIRECTORY"));
-
 		openDirectoryBtn.OnSubmit += () =>
 		{
 			Process.Start(translationDir.ToString());
 		};
 
 		menu.Add(openDirectoryBtn);
+
+		var reloadBtn = new TextButton(Text.Localized("OPTION_RELOAD_TRANSLATION"));
+		reloadBtn.OnSubmit += () =>
+		{
+			var uiManager = UIManager.instance;
+			if (uiManager == null)
+			{
+				return;
+			}
+
+			var languageOption = uiManager.transform.Find("UICanvas/GameOptionsMenuScreen/Content/LanguageSetting/LanguageOption");
+			if (languageOption == null)
+			{
+				return;
+			}
+
+			if (languageOption.TryGetComponent<MenuLanguageSetting>(out var menuLanguageSetting))
+			{
+				menuLanguageSetting.UpdateLanguageSetting();
+			}
+		};
+
+		menu.Add(reloadBtn);
 
 		var menuBtn = new TextButton(Name);
 		menuBtn.OnSubmit += () =>
